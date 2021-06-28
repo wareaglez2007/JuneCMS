df8b7f5f5a2efa54223df7dd5d56ae3350bda1c2
rostom sahakian <rostomsahakian@rostoms-Air.attlocal.net>
2021-06-27 17:34:38 -0700

    RS adding laravel files

---
+++ app/Http/Controllers/WebpagesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\webpages;
+use Illuminate\Http\Request;
+
+class WebpagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\webpages  $webpages
+     * @return \Illuminate\Http\Response
+     */
+    public function show(webpages $webpages)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\webpages  $webpages
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(webpages $webpages)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\webpages  $webpages
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, webpages $webpages)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\webpages  $webpages
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(webpages $webpages)
+    {
+        //
+    }
+}
---
+++ database/factories/WebpagesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\webpages;
+use Faker\Generator as Faker;
+
+$factory->define(webpages::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
